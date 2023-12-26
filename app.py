2533c35b93fe3560b3e44abbe4d5370c7e3c238f
Shubham Negi <shubham64negi@gmail.com>
2023-12-26 12:42:04 +0530

    made the web pages and the model file

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import Flask, request
+from movie_prediction import predict_probabilities
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World"
+def home():
+    return render_template("home.html")
+
+@app.route('/prediction')
+def predict():
+   # Get input from the Flask app
+   #actor = request.json['actor']
+   actor = "Chris Pratt"
+   #director = request.json['director']
+   director = "Colin Trevorrow"
+   #genre = request.json['genre']
+   genre = "Action"
+
+   # Predict probabilities
+   probabilities = predict_probabilities(actor, director, genre)
+
+   # Evaluate probabilities
+   if probabilities[0][0] > probabilities[0][1]:
+    result = 'Hit'
+    probability = probabilities[0][0] * 100
+   else:
+    result = 'Flop'
+    probability = probabilities[0][1] * 100
+
+   # Render the appropriate HTML file
+   return render_template('prediction.html', result=result, probability=probability)
+
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ movie_prediction.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
+# Load the dataset
+df = pd.read_csv('dataset.csv')
+
+# Encode the categorical values
+le = LabelEncoder()
+df['actor'] = le.fit_transform(df['actor'])
+mapping_actor = dict(zip(le.classes_, range(len(le.classes_))))
+df['director'] = le.fit_transform(df['director'])
+mapping_director = dict(zip(le.classes_, range(len(le.classes_))))
+df['genre'] = le.fit_transform(df['genre'])
+mapping_genre = dict(zip(le.classes_, range(len(le.classes_))))
+
+# Convert the continuous label into a binary label
+df['label'] = df['label'].apply(lambda x: 1 if x >= 0.5 else 0)
+
+# Train the model
+X = df.drop(columns = ['label'])
+y = df['label']
+
+model = LogisticRegression(max_iter = 1000)
+model.fit(X, y)
+
+# Function to predict probabilities
+def predict_probabilities(actor, director, genre):
+   # Convert input into their respective encodings
+   actor_encoded = mapping_actor[actor]
+   director_encoded = mapping_director[director]
+   genre_encoded = mapping_genre[genre]
+
+   # Create a list of encoded features
+   encoded_features = [actor_encoded, director_encoded, genre_encoded]
+
+   # Feed the encoded features into the model and output the probabilities
+   probabilities = model.predict_proba([encoded_features])
+
+   return probabilities
