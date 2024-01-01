d193175c464f2082547125d068d62d4043376ace
Shubham Negi <shubham64negi@gmail.com>
2024-01-01 18:02:17 +0530

    altered the dataset for db push

--- database.py
+++ database.py
@@ -1,30 +1,40 @@
+# extract the credits.rar file before running this code
+
 import pandas as pd
 from sqlalchemy import create_engine
 from mysql.connector import connect, Error
 
 try:
-   connection = connect(
-       host='localhost',
-       user='Entro01',
-       password='password',
-       database='store'
-   )
-   if connection.is_connected():
-       db_info = connection.get_server_info()
-       print("Connected to MySQL Server version ", db_info)
+ connection = connect(
+     host='localhost',
+     user='Entro01',
+     password='password',
+     database='store'
+ )
+ if connection.is_connected():
+     db_info = connection.get_server_info()
+     print("Connected to MySQL Server version ", db_info)
 except Error as e:
-   print("Error while connecting to MySQL", e)
+ print("Error while connecting to MySQL", e)
 finally:
-   if (connection.is_connected()):
-       connection.close()
-       print("MySQL connection is closed")
+ if (connection.is_connected()):
+     connection.close()
+     print("MySQL connection is closed")
 
-df1 = pd.read_csv('model-files\movie_data.tsv', sep='\t', dtype={4: str})
-df2 = pd.read_csv('model-files\work_data.tsv', sep='\t', dtype={4: str})
+# Load the pickled DataFrame
+df = pd.read_pickle('model-files\credits.pkl')
 
-df1 = df1.drop(['originalTitle', 'isAdult', 'startYear', 'endYear', 'runtimeMinutes'], axis=1)
-df2 = df2.drop(['birthYear', 'deathYear'], axis=1)
+# Convert the lists in the 'knownForTitles' column into a format that can be stored in the MySQL database
+df['knownForTitles'] = df['knownForTitles'].apply(lambda x: ', '.join(str(item) for item in x))
 
+# Save the DataFrame as an HDF file
+df.to_hdf('credits.hdf', key='df')
+
+# Load the DataFrame from the HDF file
+df = pd.read_hdf('credits.hdf', 'df')
+
+# Create a SQLAlchemy engine
 engine = create_engine('mysql+mysqlconnector://Entro01:password@localhost/store')
-df1.to_sql('table1', engine, if_exists='replace', index=False)
-df2.to_sql('table2', engine, if_exists='replace', index=False)
+
+# Insert the DataFrame into the database
+df.to_sql('credits', engine, if_exists='append', index=False)
--- movie_info.py
+++ movie_info.py
@@ -17,3 +17,15 @@ finally:
         connection.close()
         print("MySQL connection is closed")
 
+from transformers import pipeline
+
+# Initialize the generator
+generator = pipeline('text-generation', model='mistralai/Mixtral-8x7B-Instruct-v0.1')
+
+# Define your inputs
+inputs = f"The cast includes Chris Pratt, directed by Steven Spielberg. If they were to release a movie together today, it would most likely be a hit. Write a short paragraph explaining why by mentioning their past works."
+
+# Generate text
+output = generator(inputs, max_length=1000)[0]['generated_text']
+
+print(output)
