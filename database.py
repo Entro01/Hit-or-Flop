b14f36c6ca8b0bcd2024e40267ee559261214c8b
Shubham Negi <shubham64negi@gmail.com>
2024-01-03 12:20:53 +0530

    modified and trimmed the dataset and fixed the daabase push code

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
-# extract the credits.rar file before running this code
-
 import pandas as pd
 from sqlalchemy import create_engine
 from mysql.connector import connect, Error
+from tqdm import tqdm
 
 try:
  connection = connect(
@@ -21,20 +20,20 @@ finally:
      connection.close()
      print("MySQL connection is closed")
 
-# Load the pickled DataFrame
-df = pd.read_pickle('model-files\credits.pkl')
-
-# Convert the lists in the 'knownForTitles' column into a format that can be stored in the MySQL database
-df['knownForTitles'] = df['knownForTitles'].apply(lambda x: ', '.join(str(item) for item in x))
-
-# Save the DataFrame as an HDF file
-df.to_hdf('credits.hdf', key='df')
-
-# Load the DataFrame from the HDF file
-df = pd.read_hdf('credits.hdf', 'df')
-
 # Create a SQLAlchemy engine
 engine = create_engine('mysql+mysqlconnector://Entro01:password@localhost/store')
 
-# Insert the DataFrame into the database
-df.to_sql('credits', engine, if_exists='append', index=False)
+# Define the chunk size
+chunk_size = 10000 # Adjust this value depending on your available memory
+
+# Get the total number of rows in the CSV file
+total_rows = sum(1 for _ in open('model-files/datasets/credits.csv', 'r', encoding='utf-8')) - 1 # minus the header
+
+# Initialize a progress bar
+with tqdm(total=total_rows, unit='rows') as pbar:
+   # Loop over the chunks of the DataFrame
+   for chunk in pd.read_csv('model-files/datasets/credits.csv', chunksize=chunk_size):
+       # Insert the chunk into the database
+       chunk.to_sql('test', engine, if_exists='append', index=False)
+       # Update the progress bar
+       pbar.update(len(chunk))
